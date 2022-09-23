657e98400f1ba0bb1289d828a90a743b678afef4
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2022-09-23 15:59:21 -0300

    Adjusted trad

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -14,7 +14,7 @@
       <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('home') }}">
           <i class="material-icons">dashboard</i>
-            <p>{{ __('Dashboard') }}</p>
+            <p>Dashboard</p>
         </a>
       </li>
       <!-- <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
